2fedd2e45fc70d5a8170bbf647367f1a46695851
ThanhPhu <htp1912@gmail.com>
2019-05-17 03:35:14 +0700

    update script

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const scrapeIt = require("scrape-it")
 
 var app = express();
 
@@ -65,6 +66,12 @@ var urlRequest = 'https://www.booking.com/markers_on_map?aid=397594;label=gog235
 // var urlRequest = 'https://www.booking.com/markers_on_map?aid=397594;label=gog235jc-1FCAEoggI46AdIKlgDaPQBiAEBmAEquAEXyAEM2AEB6AEB-AECiAIBqAIDuAKRh5TmBcACAQ;sid=99b9cba327afaf19199ed9a98964ba8d;srpvid=faf368c0f39f000e&;aid=397594;dest_id=-3712045;dest_type=city;sr_id=;ref=searchresults;limit=90;stype=1;lang=vi;ssm=1;ngp=1;room1=A,A;maps_opened=1;esf=1;nflt=;sr_countrycode=vn;sr_lat=;sr_long=;dba=1;dbc=1;mar=0.3;srh=3869335,2569018,2827847,3401237,1172239,1334692,2201858,2109619,2869102,2551657,2859837,3833819,2132505,2651165,3727120,4003999;somp=1;somp=1;mdimb=1%20;tp=1%20;img_size=270x200%20;avl=1%20;nor=1%20;spc=1%20;rmd=1%20;slpnd=1%20;sbr=1;BBOX=108.4237101674081,11.940734586569711,108.45121890306484,11.944471381358039&_=1557672838598';
 // var urlRequest = 'https://www.booking.com/markers_on_map?aid=397594;label=gog235jc-1FCAEoggI46AdIKlgDaPQBiAEBmAEquAEXyAEM2AEB6AEB-AECiAIBqAIDuAKRh5TmBcACAQ;sid=99b9cba327afaf19199ed9a98964ba8d;srpvid=2f007a6c535e0295&;aid=397594;dest_id=-3712045;dest_type=city;sr_id=;ref=searchresults;limit=20;stype=1;lang=vi;ssm=1;ngp=1;room1=A,A;maps_opened=1;esf=1;nflt=;sr_countrycode=vn;sr_lat=;sr_long=;dba=1;dbc=1;mar=0.3;srh=2827847,2569018,3869335,3401237,1172239,1334692,2201858,2109619,2869102,2551657,2859837,3833819,2132505,2651165,3727120,4003999;somp=1;somp=1;mdimb=1%20;tp=1%20;img_size=270x200%20;avl=1%20;nor=1%20;spc=1%20;rmd=1%20;slpnd=1%20;sbr=1;BBOX=108.44455883217165,11.934876886152267,108.47206756782839,11.94596140047741&_=1557941099043';
 // var urlRequest = 'https://www.booking.com/markers_on_map?aid=397594;label=gog235jc-1DCAEoggI46AdIKlgDaPQBiAEBmAEquAEXyAEM2AED6AEB-AECiAIBqAIDuAKRh5TmBcACAQ;sid=99b9cba327afaf19199ed9a98964ba8d;srpvid=67376e380f5e02c9&;aid=397594;dest_id=-3712045;dest_type=city;sr_id=;ref=searchresults;limit=20;stype=1;lang=vi;ssm=1;ngp=1;room1=A,A;maps_opened=1;esf=1;nflt=;sr_countrycode=vn;sr_lat=;sr_long=;dba=1;dbc=1;mar=0.3;srh=2827847,2569018,3869335,3401237,1172239,1334692,2201858,2109619,2869102,2551657,2859837,3833819,2132505,2651165,3727120,4003999;somp=1;somp=1;mdimb=1%20;tp=1%20;img_size=270x200%20;avl=1%20;nor=1%20;spc=1%20;rmd=1%20;slpnd=1%20;sbr=1;BBOX=108.4237101674081,11.936997740241612,108.45121890306484,11.948208124591456&_=1558021249771';
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
+
 (async function () {
     try {
         console.log("sql connecting......")
@@ -76,39 +83,67 @@ var urlRequest = 'https://www.booking.com/markers_on_map?aid=397594;label=gog235
                 const bodyJson = JSON.parse(body);
                 var hotels = [];
                 bodyJson.b_hotels.forEach(item => {
+
+                });
+                asyncForEach(bodyJson.b_hotels, async (item) => {
                     console.log(item.b_hotel_title)
                     const urlImage = image_url + item.b_image_url;
                     const urls = urlImage.split("/");
                     const lastName = urls[urls.length - 1];
-                    download(image_url + item.b_image_url, 'images/'+lastName, function () {
-                         // console.log('done');
-                     });
-                    const itemHotel = {
-                        col: [
-                            'Da lat',
-                            'Da lat',
-                            'description',
-                            item.b_hotel_title,
-                            lastName,
-                            parseFloat(item.b_latitude),
-                            parseFloat(item.b_longitude)
-                        ]
-                    };
-                    hotels.push(itemHotel)
-                });
-                resolve(hotels)
+                   await download(image_url + item.b_image_url, 'images/'+lastName, function () {
+                        // console.log('done');
+                    });
+                   await scrapeIt('https://www.booking.com'+item.b_url, {
+                        address: ".js-hp_address_subtitle"
+                        , desc: "#summary"
+                        // Fetch the blog pages
+                        , roomNames: {
+                            listItem: "tr.hprt-table-last-row",
+                            name : 'roomNames'
+                            , data: {
+                                title: ".hprt-roomtype-icon-link"
+                            }
+                        }
+                        , avatar: {
+                            selector: "#photo_wrapper > div.hp-gallery-slides.hp-gallery-top.slick-initialized.slick-slider > div > div > div.slick-slide.slick-active > img"
+                            , attr: "src"
+                        }
+                    }).then(({ data, response }) => {
+                        console.log(`Status Code: ${response.statusCode}`)
+                        const itemHotel = {
+                            col: [
+                                data.address,
+                                data.address.split(/[,]/)[data.address.split(/[,]/).length-2],
+                                data.desc,
+                                item.b_hotel_title,
+                                lastName,
+                                parseFloat(item.b_latitude),
+                                parseFloat(item.b_longitude),
+                                new Date(),
+                                new Date(),
+                                'nguyenvana',
+                            ]
+                        };
+                        hotels.push(itemHotel)
+                    })
+                }).then(function () {
+                    resolve(hotels)
+                })
             })
         });
-        console.log('Log info for hotels: ', hotelsData);
         const table = new sql.Table('hotel') // or temporary table, e.g. #temptable
         table.create = true;
-        table.columns.add('address', sql.NVarChar(255))
+        table.columns.add('address', sql.NVarChar(sql.MAX))
         table.columns.add('city', sql.VarChar(255))
-        table.columns.add('description', sql.NVarChar(255))
+        table.columns.add('description', sql.NVarChar(sql.MAX))
         table.columns.add('hotelName', sql.NVarChar(255))
         table.columns.add('images', sql.NVarChar(255))
         table.columns.add('lat', sql.Float())
         table.columns.add('lng', sql.Float())
+        table.columns.add('createdBy', sql.DateTime2())
+        table.columns.add('createdDate', sql.DateTime2())
+        table.columns.add('modifiedBy', sql.VarChar(255))
+
         hotelsData.forEach((item, index) => {
                 table.rows.add(
                     item.col[0],
@@ -118,12 +153,16 @@ var urlRequest = 'https://www.booking.com/markers_on_map?aid=397594;label=gog235
                     item.col[4],
                     item.col[5],
                     item.col[6],
+                    item.col[7],
+                    item.col[8],
+                    item.col[9],
                 );
         })
         console.log(table);
         let result = await pool.request()
             .bulk(table)  // subject is my database table name
         console.log(result)
+
     } catch (err) {
         console.log(err);
     }
