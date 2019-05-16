1870f50d5e50e15928c71a63824befc21c550955
ThanhPhu <htp1912@gmail.com>
2019-05-16 22:34:47 +0700

    update size

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ var sql = require('mssql');
 var image_url = 'https://q-ak.bstatic.com';
 var request = require('request');
 // var urlRequest = 'https://www.booking.com/markers_on_map?aid=397594;label=gog235jc-1FCAEoggI46AdIKlgDaPQBiAEBmAEquAEXyAEM2AEB6AEB-AECiAIBqAIDuAKRh5TmBcACAQ;sid=99b9cba327afaf19199ed9a98964ba8d;srpvid=faf368c0f39f000e&;aid=397594;dest_id=-3712045;dest_type=city;sr_id=;ref=searchresults;limit=90;stype=1;lang=vi;ssm=1;ngp=1;room1=A,A;maps_opened=1;esf=1;nflt=;sr_countrycode=vn;sr_lat=;sr_long=;dba=1;dbc=1;mar=0.3;srh=3869335,2569018,2827847,3401237,1172239,1334692,2201858,2109619,2869102,2551657,2859837,3833819,2132505,2651165,3727120,4003999;somp=1;somp=1;mdimb=1%20;tp=1%20;img_size=270x200%20;avl=1%20;nor=1%20;spc=1%20;rmd=1%20;slpnd=1%20;sbr=1;BBOX=108.4237101674081,11.940734586569711,108.45121890306484,11.944471381358039&_=1557672838598';
-var urlRequest = 'https://www.booking.com/markers_on_map?aid=397594;label=gog235jc-1FCAEoggI46AdIKlgDaPQBiAEBmAEquAEXyAEM2AEB6AEB-AECiAIBqAIDuAKRh5TmBcACAQ;sid=99b9cba327afaf19199ed9a98964ba8d;srpvid=2f007a6c535e0295&;aid=397594;dest_id=-3712045;dest_type=city;sr_id=;ref=searchresults;limit=90;stype=1;lang=vi;ssm=1;ngp=1;room1=A,A;maps_opened=1;esf=1;nflt=;sr_countrycode=vn;sr_lat=;sr_long=;dba=1;dbc=1;mar=0.3;srh=2827847,2569018,3869335,3401237,1172239,1334692,2201858,2109619,2869102,2551657,2859837,3833819,2132505,2651165,3727120,4003999;somp=1;somp=1;mdimb=1%20;tp=1%20;img_size=270x200%20;avl=1%20;nor=1%20;spc=1%20;rmd=1%20;slpnd=1%20;sbr=1;BBOX=108.44455883217165,11.934876886152267,108.47206756782839,11.94596140047741&_=1557941099043';
+var urlRequest = 'https://www.booking.com/markers_on_map?aid=397594;label=gog235jc-1FCAEoggI46AdIKlgDaPQBiAEBmAEquAEXyAEM2AEB6AEB-AECiAIBqAIDuAKRh5TmBcACAQ;sid=99b9cba327afaf19199ed9a98964ba8d;srpvid=2f007a6c535e0295&;aid=397594;dest_id=-3712045;dest_type=city;sr_id=;ref=searchresults;limit=20;stype=1;lang=vi;ssm=1;ngp=1;room1=A,A;maps_opened=1;esf=1;nflt=;sr_countrycode=vn;sr_lat=;sr_long=;dba=1;dbc=1;mar=0.3;srh=2827847,2569018,3869335,3401237,1172239,1334692,2201858,2109619,2869102,2551657,2859837,3833819,2132505,2651165,3727120,4003999;somp=1;somp=1;mdimb=1%20;tp=1%20;img_size=270x200%20;avl=1%20;nor=1%20;spc=1%20;rmd=1%20;slpnd=1%20;sbr=1;BBOX=108.44455883217165,11.934876886152267,108.47206756782839,11.94596140047741&_=1557941099043';
 
 (async function () {
     try {
@@ -108,7 +108,6 @@ var urlRequest = 'https://www.booking.com/markers_on_map?aid=397594;label=gog235
         table.columns.add('lat', sql.Float())
         table.columns.add('lng', sql.Float())
         hotelsData.forEach((item, index) => {
-            if(index<20){
                 table.rows.add(
                     item.col[0],
                     item.col[1],
@@ -118,7 +117,6 @@ var urlRequest = 'https://www.booking.com/markers_on_map?aid=397594;label=gog235
                     item.col[5],
                     item.col[6],
                 );
-            }
         })
         console.log(table);
         let result = await pool.request()
